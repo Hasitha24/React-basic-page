8088aa07f1b869eefc076c39344c89046237537d
Hasitha24 <hasithak002@gmail.com>
2023-08-10 01:19:20 +0530

    Initiated React page building

---
+++ MainContent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function MainContent() {
+    return(
+        <div>
+        <h1>My page</h1>
+        <ul>
+            <li>Hasitha</li>
+            <li>Kapugamage</li>
+        </ul>
+        </div>
+    )
+    }
